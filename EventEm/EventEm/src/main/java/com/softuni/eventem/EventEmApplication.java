9fcf6c3a1ef50fdbbc48e70ca1a4206fc3ce580a
Hristiyan Delisyarov <h.delisiarov99@gmail.com>
2023-11-30 23:52:16 +0200

    Initial commit

---
+++ EventEm/EventEm/src/main/java/com/softuni/eventem/EventEmApplication.java
@@ -0,0 +1,13 @@
+package com.softuni.eventem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventEmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventEmApplication.class, args);
+	}
+
+}
