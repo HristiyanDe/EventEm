98a7ceead616f8ac0e67ad277cea0d58183a439e
Hristiyan Delisyarov <h.delisiarov.master@gmail.com>
2023-12-16 08:50:24 +0200

    Swagger UI implementation, no API detailed descriptions yet

---
+++ EventEm/EventEm/src/main/java/com/softuni/eventem/constants/OpenAPIInfo.java
@@ -0,0 +1,11 @@
+package com.softuni.eventem.constants;
+
+public class OpenAPIInfo {
+
+	public static final String API_URL = "http://localhost:8080";
+	public static final String API_TITLE= "EventEm API";
+	public static final String API_DESCRIPTION = """
+	                                             EventEm is a student REST application aiming to replicate(a part of and additionally) Eventim's online ticket store""";
+
+
+}
