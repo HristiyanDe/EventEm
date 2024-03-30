a0c0c7ff6e1d0b1927a70603c20cfa7234221d25
Hristiyan Delisyarov <h.delisiarov.master@gmail.com>
2024-03-30 02:11:09 +0200

    feat: implement getting organizations associated with user

--- EventEm/EventEm/src/main/java/com/softuni/eventem/repositories/OrganizationRepository.java
+++ EventEm/EventEm/src/main/java/com/softuni/eventem/repositories/OrganizationRepository.java
@@ -2,7 +2,13 @@ package com.softuni.eventem.repositories;
 
 import com.softuni.eventem.entities.OrganizationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrganizationRepository extends JpaRepository<OrganizationEntity, Long> {
+  @Query("SELECT o FROM OrganizationEntity o JOIN o.users u WHERE u.id = :userId")
+  List<OrganizationEntity> findAllByUserId(@Param("userId") Long userId);
 
 }
--- EventEm/EventEm/src/main/java/com/softuni/eventem/services/OrganizationService.java
+++ EventEm/EventEm/src/main/java/com/softuni/eventem/services/OrganizationService.java
@@ -1,10 +1,15 @@
 package com.softuni.eventem.services;
 
 import com.softuni.eventem.entities.OrganizationEntity;
+import com.softuni.eventem.entities.dto.OrganizationDTO;
 import com.softuni.eventem.entities.request.OrganizationRequest;
+
+import java.util.List;
 
 public interface OrganizationService {
   OrganizationEntity createOrganization(OrganizationRequest organizationRequest);
 
   OrganizationEntity getOrganizationById(Long organizationId);
+
+  List<OrganizationDTO> getOrganizationsByUserId(Long id);
 }
