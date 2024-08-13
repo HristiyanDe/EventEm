08ea9dbb2eca8d925640d4c87cb51812039d9556
Hristiyan Delisyarov <h.delisiarov.master@gmail.com>
2024-08-13 23:37:37 +0300

    Implement mobile layout for user profile, define types for profile update requests

--- eventem-react/src/components/UserComponent/UserProfile.tsx
+++ eventem-react/src/components/UserComponent/UserProfile.tsx
@@ -1,11 +1,38 @@
 import React from "react";
 import { useAuth } from "../../auth/AuthContext";
-import { Typography, Box, CssBaseline } from "@mui/material";
+import { Typography, Box, CssBaseline, TextField, Icon, Button } from "@mui/material";
 import  { ThemeProvider } from "@mui/material/styles";
 import { Container } from "@mui/system";
 import { defaultTheme } from '../RegisterComponent/RegisterComponent';
+import { AccountCircle } from "@mui/icons-material";
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import { useState } from "react";
+import { UpdateUserRequest } from "../../models/UpdateUserRequest";
 const UserProfileComponent: React.FC = () => {
     const { token, user} = useAuth();
+    const [isEditing, setIsEditing] = useState(false);
+    const handleEditClick = () => {
+      setIsEditing(!isEditing);
+  };
+  const [formData, setFormData] = useState<UpdateUserRequest>({
+    username: null,
+    password: null,
+    firstName: null,
+    lastName: null,
+    city: null,
+    address: null,
+    phone: null,
+    email: null,
+
+});
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const {name, value} = e.target;
+    setFormData({
+        ...formData,
+        [name]:value,
+    });
+};
   return (
     <ThemeProvider theme={defaultTheme}>
           <Container component="main" maxWidth="xs">
@@ -15,9 +42,31 @@ const UserProfileComponent: React.FC = () => {
                 display: 'flex',
                 flexDirection: 'column',
                 alignItems: 'center',}}>
+                    <Icon fontSize="large">
+                    <AccountCircle fontSize="large"/>
+                    </Icon>
                 <Typography component="h1" variant="h5">
-                    User Profile
+                    Profile
                 </Typography>
+                <Box sx={{marginTop: 3, marginBottom: 3, padding: 2, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <Button onClick={handleEditClick} id="edit-profile-button" variant="outlined" endIcon={<ModeEditIcon/>}> Button</Button>
+                </Box>
+                <Box component="form">
+                    {/*<TextField sx={{paddingBottom: 2}} fullWidth id="firstName" label="First Name" variant="outlined" value={user?.username} />*/}
+                    <TextField disabled={!isEditing} sx={{paddingBottom: 2}} fullWidth id="firstName" label="First Name" variant="outlined" value={user?.firstName} />
+                    <TextField disabled={!isEditing} sx={{paddingBottom: 2}} fullWidth id="lastName" label="Last Name" variant="outlined" value={user?.lastName} />
+                    <TextField disabled={!isEditing} sx={{paddingBottom: 2}} fullWidth id="email" label="Email" variant="outlined" value={user?.email} />
+                    <TextField disabled={!isEditing} sx={{paddingBottom: 2}} fullWidth id="address" label="Address" variant="outlined" value={user?.address} />
+                    <Button
+                    id="submit-profile-button"
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Edit profile
+                </Button>
+                </Box>
                 <Typography component="h1" variant="h5">
                     {user?.firstName}
                 </Typography>
@@ -27,7 +76,8 @@ const UserProfileComponent: React.FC = () => {
                 <Typography component="h1" variant="h5">
                     {user?.address}
                 </Typography>
-            </Box>
+                </Box>
+                
             </Container>
             </ThemeProvider>
 );
---
+++ eventem-react/src/models/UpdateUserRequest.ts
@@ -0,0 +1,10 @@
+export interface UpdateUserRequest{
+    username: string | null;
+    password: string | null;
+    firstName: string | null;
+    lastName: string | null;
+    city: string | null;
+    address: string | null;
+    phone: string | null;
+    email: string | null;
+    }
